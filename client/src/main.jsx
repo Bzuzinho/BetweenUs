8d7508396c13bc7529ba338b2a1765b558872a99
Bzuzinho <ricardo.jgvf@gmail.com>
2026-07-20 15:46:02 +0100

    Provide internationalisation context to the client

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,14 +3,17 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import ErrorBoundary from './ErrorBoundary.jsx'
+import { I18nProvider } from './i18n/I18nContext.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ErrorBoundary>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <I18nProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </I18nProvider>
     </ErrorBoundary>
   </React.StrictMode>
 )
