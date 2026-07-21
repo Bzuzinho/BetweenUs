4c318212726043dcd50f1dce59139e7e96a5a874
Bzuzinho <ricardo.jgvf@gmail.com>
2026-07-21 09:12:00 +0100

    ci: simplify language test migration script

--- server/scripts/applyPreferredLanguageTestMigration.js
+++ server/scripts/applyPreferredLanguageTestMigration.js
@@ -7,21 +7,6 @@ async function main() {
     ALTER TABLE "User"
     ADD COLUMN IF NOT EXISTS "preferredLanguage" TEXT NOT NULL DEFAULT 'pt-PT'
   `)
-
-  await prisma.$executeRawUnsafe(`
-    DO $$
-    BEGIN
-      IF NOT EXISTS (
-        SELECT 1 FROM pg_constraint
-        WHERE conname = 'User_preferredLanguage_check'
-      ) THEN
-        ALTER TABLE "User"
-        ADD CONSTRAINT "User_preferredLanguage_check"
-        CHECK ("preferredLanguage" IN ('pt-PT', 'en', 'fr'));
-      END IF;
-    END
-    $$;
-  `)
 }
 
 main()
