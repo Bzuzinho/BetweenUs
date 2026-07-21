a70fb8edc851a2da68e1b6a377c8dbdc698f0545
Bzuzinho <ricardo.jgvf@gmail.com>
2026-07-21 15:10:37 +0100

    Add admin user creation translations

---
+++ client/src/i18n/adminUserCreationTranslations.js
@@ -0,0 +1,5 @@
+export const adminUserCreationTranslations = {
+  'pt-PT': { admin:{ userCreate:{ title:'Criar utilizador', email:'Email *', password:'Palavra-passe inicial *', reason:'Motivo *', required:'Email, palavra-passe e motivo são obrigatórios.', error:'Não foi possível criar o utilizador.', create:'Criar utilizador' } } },
+  en: { admin:{ userCreate:{ title:'Create user', email:'Email *', password:'Initial password *', reason:'Reason *', required:'Email, password and reason are required.', error:'The user could not be created.', create:'Create user' } } },
+  fr: { admin:{ userCreate:{ title:'Créer un utilisateur', email:'E-mail *', password:'Mot de passe initial *', reason:'Motif *', required:'L’e-mail, le mot de passe et le motif sont obligatoires.', error:'L’utilisateur n’a pas pu être créé.', create:'Créer l’utilisateur' } } },
+}
