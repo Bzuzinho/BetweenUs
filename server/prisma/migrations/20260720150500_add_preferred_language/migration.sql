131cb8c4b08a3e1a04e06ca9bb233a7c4c567f99
Bzuzinho <ricardo.jgvf@gmail.com>
2026-07-21 09:27:23 +0100

    fix: migrate mapped users table

--- server/prisma/migrations/20260720150500_add_preferred_language/migration.sql
+++ server/prisma/migrations/20260720150500_add_preferred_language/migration.sql
@@ -1,8 +1,8 @@
 -- Internacionalização da conta.
 -- Utilizadores existentes permanecem em Português de Portugal.
-ALTER TABLE "User"
+ALTER TABLE "users"
 ADD COLUMN "preferredLanguage" TEXT NOT NULL DEFAULT 'pt-PT';
 
-ALTER TABLE "User"
-ADD CONSTRAINT "User_preferredLanguage_check"
+ALTER TABLE "users"
+ADD CONSTRAINT "users_preferredLanguage_check"
 CHECK ("preferredLanguage" IN ('pt-PT', 'en', 'fr'));
